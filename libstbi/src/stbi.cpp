300f075ce5f910ac18ada6290cf5d1ece5e17108
Thomas Müller <thomas94@gmx.net>
2019-11-15 16:00:34 +0900

    Initial implementation of C# wrapper around native lib

--- libstbi/src/stbi.cpp
+++ libstbi/src/stbi.cpp
@@ -1,14 +1,19 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define EXPORT __attribute__((visibility("default")))
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#elif __APPLE__
+    #define EXPORT __attribute__((visibility("default")))
+#else
+    #define EXPORT
+#endif
 
 extern "C" {
-    EXPORT bool stbiInfo(const unsigned char* data, int64_t len, int* w, int* h, int* nChannels) {
-        return stbi_info_from_memory(data, len, w, h, nChannels) == 1;
-    }
-
-    EXPORT bool stbiLoad(unsigned char* dst, const unsigned char* data, int64_t len, int nDesiredChannels) {
+    EXPORT bool StbiLoadFromMemoryIntoBuffer(const unsigned char* data, int64_t len, int nDesiredChannels, unsigned char* dst) {
+        // Dummy variables that are not going to be used. Returning them is unnecessary, because the probided destination buffer
+        // needs to already have the correct size, hence the caller must have already requested the width, height, and number of
+        // channels beforehand.
         int width, height, nChannels;
         unsigned char* tmp = stbi_load_from_memory(data, len, &width, &height, &nChannels, nDesiredChannels);
 
@@ -25,11 +30,15 @@ extern "C" {
         return true;
     }
 
-    EXPORT unsigned char* stbiLoadRaw(const unsigned char* data, int64_t len, int* w, int* h, int* nChannels, int nDesiredChannels) {
+    EXPORT bool StbiInfoFromMemory(const unsigned char* data, int64_t len, int* w, int* h, int* nChannels) {
+        return stbi_info_from_memory(data, len, w, h, nChannels) == 1;
+    }
+
+    EXPORT unsigned char* StbiLoadFromMemory(const unsigned char* data, int64_t len, int* w, int* h, int* nChannels, int nDesiredChannels) {
         return stbi_load_from_memory(data, len, w, h, nChannels, nDesiredChannels);
     }
 
-    EXPORT void stbiFreeRaw(unsigned char* pixels) {
+    EXPORT void StbiFree(unsigned char* pixels) {
         stbi_image_free(pixels);
     }
 }
---
+++ stbi-sharp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace StbiSharp
+{
+    unsafe public class StbiImage : IDisposable
+    {
+        private byte* data;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int NumChannels { get; private set; }
+
+        public ReadOnlySpan<byte> Data => new ReadOnlySpan<byte>(data, Width * Height * NumChannels);
+
+        public StbiImage(byte* data, int width, int height, int numChannels)
+        {
+            this.data = data;
+
+            Width = width;
+            Height = height;
+            NumChannels = numChannels;
+        }
+
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (data != null)
+            {
+                Stbi.Free(data);
+                data = null;
+            }
+        }
+
+        ~StbiImage()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+
+    public class Stbi
+    {
+        [DllImport("stbi")]
+        unsafe public static extern bool LoadFromMemoryIntoBuffer(byte* data, long len, byte* dst);
+
+        unsafe public static void LoadFromMemoryIntoBuffer(byte[] data, byte[] dst)
+        {
+            fixed (byte* address = data)
+                fixed (byte* dstAddress = dst)
+                    if (!LoadFromMemoryIntoBuffer(address, data.Length, dstAddress))
+                        throw new ArgumentException($"STBI could not load an image from the provided {nameof(data)}.");
+        }
+
+        public static void LoadFromMemoryInfoBuffer(MemoryStream m, byte[] dst) =>
+            LoadFromMemoryIntoBuffer(m.GetBuffer(), dst);
+
+        [DllImport("stbi")]
+        unsafe public static extern bool InfoFromMemory(byte* data, long len, out int width, out int height, out int numChannels);
+
+        unsafe public static bool InfoFromMemory(byte[] data, out int width, out int height, out int numChannels)
+        {
+            fixed (byte* address = data)
+                return InfoFromMemory(address, data.Length, out width, out height, out numChannels);
+        }
+
+        public static bool InfoFromMemory(MemoryStream m, out int width, out int height, out int numChannels)
+            => InfoFromMemory(m.GetBuffer(), out width, out height, out numChannels);
+
+        [DllImport("stbi")]
+        unsafe public static extern byte* LoadFromMemory(byte* data, long len, out int width, out int height, out int numChannels, int desiredNumChannels);
+
+        [DllImport("stbi")]
+        unsafe public static extern void Free(byte* pixels);
+
+        unsafe public static StbiImage LoadFromMemory(byte[] data, int desiredNumChannels)
+        {
+            fixed (byte* address = data) {
+                byte* pixels = LoadFromMemory(address, data.Length, out int width, out int height, out int numChannels, desiredNumChannels);
+                if (pixels == null) {
+                    throw new ArgumentException($"STBI could not load an image from the provided {nameof(data)}.");
+                }
+
+                return new StbiImage(pixels, width, height, desiredNumChannels == 0 ? numChannels : desiredNumChannels);
+            }
+        }
+
+        public static StbiImage LoadFromMemory(MemoryStream m, int desiredNumChannels)
+            => LoadFromMemory(m.GetBuffer(), desiredNumChannels);
+    }
+}
